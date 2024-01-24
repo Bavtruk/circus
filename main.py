d80df00ca85f00b61a1d6dff83af9dd9d40cf51a
Aleksandrs Ļinovs <you@example.com>
2024-01-24 10:22:51 +0200

    Aleksandrs Ļinovs

--- main.py
+++ main.py
@@ -24,3 +24,80 @@
 # Github Fork (repozitorija kopija) - https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo
 # Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
 
+#Aleksandrs Ļinovs
+player_location = 1
+ai_location = 1
+import random
+def player_move():
+    global player_location
+    print("Your turn throw cube")
+    metiens=input ()
+    if metiens == "mest":
+        kaulins = random.randint(1 , 6)
+        print("Tu uzmetāt:", kaulins)
+        player_location = player_location + kaulins
+        if player_location == 18:
+            player_location = 7
+            print("Unlucky,you go to 7")
+        elif player_location == 67:
+            player_location = 46
+            print("Unlucky,you go to 46")
+        elif player_location == 74:
+            player_location = 63
+            print("Unlucky,you go to 63") 
+        elif player_location == 80:
+            player_location = 69
+            print("Unlucky,you go to 69")
+        
+        elif player_location == 15:
+            player_location = 24
+            print("Ļucky,you go to 24")
+        elif player_location == 39:
+            player_location = 48
+            print("Ļucky,you go to 48")
+        elif player_location == 33:
+            player_location = 52
+            print("Ļucky,you go to 52")
+        elif player_location == 87:
+            player_location = 96
+            print("Ļucky,you go to 96")
+        else:
+            print("Jus atrodaties uz ", player_location, ". lauciņa")
+    else:
+        print("mistake")
+        
+def ai_move():
+    global ai_location
+    print("opponent's attack")
+    kaulins = random.randint(1 , 6)
+    print("opponent uzmetāt:", kaulins)
+    ai_location = ai_location + kaulins
+    if ai_location == 18:
+            ai_location = 7
+            print("Unlucky ai, go to 7")
+    elif ai_location == 67:
+            ai_location = 46
+            print("Unlucky ai, go to 46")
+    elif ai_location == 74:
+            ai_location = 63
+            print("Unlucky ai, go to 63") 
+    elif ai_location == 80:
+            ai_location = 69
+            print("Unlucky ai, go to 69")
+        
+    elif ai_location == 15:
+            ai_location = 24
+            print("Ļucky ai, go to 24")
+    elif ai_location == 39:
+            ai_location = 48
+            print("Ļucky ai, go to 48")
+    elif ai_location == 33:
+            ai_location = 52
+            print("Ļucky ai, go to 52")
+    elif ai_location == 87:
+            ai_location = 96
+            print("Ļucky ai, go to 96")
+    else:
+            print("ai atrodaties uz ", ai_location, ". lauciņa")
+
+
